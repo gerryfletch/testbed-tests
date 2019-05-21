8d9a4d0a4ea4ec301c308695d2aedd84a1cb73d3
gerryfletch <1997gerry@gmail.com>
2019-05-21 19:17:05 +0100

    Created how an enterprise user may prepare their test setup with mocks.

--- testjunit4powermock/src/test/java/dev/testbed/dependency_new/UserServiceTest.java
+++ testjunit4powermock/src/test/java/dev/testbed/dependency_new/UserServiceTest.java
@@ -1,18 +1,38 @@
 package dev.testbed.dependency_new;
 
+import dev.testbed.FriendDao;
 import dev.testbed.TestBed;
+import dev.testbed.UserDao;
 import dev.testbed.UserServiceTestInterface;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.whenNew;
+
+@RunWith(PowerMockRunner.class)
 @PrepareForTest(UserService.class)
 public class UserServiceTest implements UserServiceTestInterface {
 
+    UserDao mockUserDao;
+    FriendDao friendDao;
+
+    @Before
+    public void setup() throws Exception {
+        mockUserDao = mock(UserDao.class);
+        whenNew(UserDao.class).withAnyArguments().thenReturn(mockUserDao);
+
+        friendDao = mock(FriendDao.class);
+        whenNew(FriendDao.class).withAnyArguments().thenReturn(friendDao);
+    }
 
     @Test
     @Override
     public void sameUserAndFriend_ThrowsBadRequestException() {
-
+        UserService userService = new TestBuilder().build();
     }
 
     @Test
