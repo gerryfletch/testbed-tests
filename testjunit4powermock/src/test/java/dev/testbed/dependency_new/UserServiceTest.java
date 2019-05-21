ab467ab53fbdd761f68ecefd0dd35b388e7a5b9c
gerryfletch <1997gerry@gmail.com>
2019-05-21 19:07:50 +0100

    Created shell of UserServiceTest for New Dependencies module.

--- testjunit4powermock/src/test/java/dev/testbed/dependency_new/UserServiceTest.java
+++ testjunit4powermock/src/test/java/dev/testbed/dependency_new/UserServiceTest.java
@@ -1,5 +1,55 @@
 package dev.testbed.dependency_new;
 
-public class UserServiceTest {
+import dev.testbed.TestBed;
+import dev.testbed.UserServiceTestInterface;
+import org.junit.Test;
+import org.powermock.core.classloader.annotations.PrepareForTest;
 
+@PrepareForTest(UserService.class)
+public class UserServiceTest implements UserServiceTestInterface {
+
+
+    @Test
+    @Override
+    public void sameUserAndFriend_ThrowsBadRequestException() {
+
+    }
+
+    @Test
+    @Override
+    public void userDoesNotExist_ThrowsUserNotExistsException() {
+
+    }
+
+    @Test
+    @Override
+    public void friendDoesNotExist_ThrowsUserNotExistsException() {
+
+    }
+
+    @Test
+    @Override
+    public void friendshipCreated_FriendshipIdReturned() {
+
+    }
+
+    @Test
+    @Override
+    public void userDao_getUser_CalledWithCallingUser() {
+
+    }
+
+    @Test
+    @Override
+    public void friendDao_createFriendship_CalledWithUsers() {
+
+    }
+
+    class TestBuilder extends TestBed<UserService, TestBuilder> {
+
+        TestBuilder() {
+            super(UserService.class);
+        }
+
+    }
 }
